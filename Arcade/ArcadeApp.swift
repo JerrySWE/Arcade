aaaa7d496b3a0d9cd607f948d54635df856b580e
JerrySWE <jiaruiwa@gmail.com>
2023-11-18 03:06:47 -0500

    Initial Commit

---
+++ Arcade/ArcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadeApp.swift
+//  Arcade
+//
+//  Created by Jerry Wang on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ArcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
